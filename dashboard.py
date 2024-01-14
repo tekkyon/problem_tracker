875a85ae83a447a4a895d4b668abd1ad6197cec2
Vladimir Diyanov <deathskilled@gmail.com>
2024-01-14 22:01:43 +0300

    added libraries

--- dashboard.py
+++ dashboard.py
@@ -546,4 +546,4 @@ hide_streamlit_style = """
                 }
                 </style>
                 """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
