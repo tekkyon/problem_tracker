7f246bd94b6e2a6ebf4beee1fa0276eda1a40d78
Vladimir Diyanov <deathskilled@gmail.com>
2024-02-03 12:28:10 +0300

    added libraries

--- dashboard.py
+++ dashboard.py
@@ -38,35 +38,35 @@ if st.session_state['password']:
         render_settings()
 
 
-# hide_streamlit_style = """
-#                 <style>
-#                 div[data-testid="stToolbar"] {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 position: fixed;
-#                 }
-#                 div[data-testid="stDecoration"] {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 position: fixed;
-#                 }
-#                 div[data-testid="stStatusWidget"] {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 position: fixed;
-#                 }
-#                 #MainMenu {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 }
-#                 header {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 }
-#                 footer {
-#                 visibility: hidden;
-#                 height: 0%;
-#                 }
-#                 </style>
-#                 """
-# st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+                <style>
+                div[data-testid="stToolbar"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stDecoration"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                div[data-testid="stStatusWidget"] {
+                visibility: hidden;
+                height: 0%;
+                position: fixed;
+                }
+                #MainMenu {
+                visibility: hidden;
+                height: 0%;
+                }
+                header {
+                visibility: hidden;
+                height: 0%;
+                }
+                footer {
+                visibility: hidden;
+                height: 0%;
+                }
+                </style>
+                """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
